55988a212f2eeecc8eaeb89b6554f1adcc328c16
Eshank Vaish <eshank.vaish@gmail.com>
2022-07-03 01:16:35 +0530

    Implemented Auto Logout

---
+++ lib/auth_service.dart
@@ -0,0 +1,12 @@
+/// For managing the authentication logic
+class AuthService {
+  bool isUserLoggedIn() {
+    // Update as per logged in logic
+    return true;
+  }
+
+  void logoutUser({String? reason}) {
+    // Logout the user
+    print('Logged Out: $reason');
+  }
+}
